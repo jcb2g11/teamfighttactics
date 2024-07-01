3410ee48128d65e231e59bd9654e677c76babdba
Cedric Bondoc <jced826@gmail.com>
2024-07-01 10:52:13 +0100

    Fix the CORS

--- backend/src/main/java/com/riot/tft/teamfighttactics/service/TFTLeagueServiceImpl.java
+++ backend/src/main/java/com/riot/tft/teamfighttactics/service/TFTLeagueServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
