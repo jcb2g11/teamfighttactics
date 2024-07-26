d6fa410977209c34965c7a874f19825f2c883849
Cedric Bondoc <jced826@gmail.com>
2024-07-26 17:38:11 +0100

    Add test profile

--- backend/src/test/java/com/riot/tft/teamfighttactics/TeamfighttacticsApplicationTests.java
+++ backend/src/test/java/com/riot/tft/teamfighttactics/TeamfighttacticsApplicationTests.java
@@ -2,8 +2,11 @@ package com.riot.tft.teamfighttactics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TeamfighttacticsApplicationTests {
 
 	@Test
